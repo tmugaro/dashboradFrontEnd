534d63f9ad515724cd2cc775905de558eca5a41f
tmugaro <naldotinashe1@gmail.com>
2023-07-14 17:16:58 -0400

    added echarts on the dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EChartsOption } from 'echarts';
+import { BloodGlucose } from './service/models/bloodGlucose';
+import { BoundElementProperty } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+  option:[EChartsOption] = {
+    title: {
+      text: 'Stacked Line'
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      data: ['FirstName', 'LastName', 'Age', 'Weight', 'Height', 'BloodGlucose','BloodPressure']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: 'FirstName',
+        type: 'line',
+        stack: 'Total',
+        data: ['tinashe', 'john', 'spardo', 'peter', 'enuice', 'portia', 'clara']
+      },
+      {
+        name: 'LastName',
+        type: 'line',
+        stack: 'Total',
+        data: ['choga', 'anna', 'masango', 'tendai', 'marcieline', 'fadzai', 'cashington']
+      },
+      {
+        name: 'Age',
+        type: 'line',
+        stack: 'Total',
+        data: [30, 32, 20, 54, 90, 33, 41]
+      },
+      {
+        name: 'Weight',
+        type: 'line',
+        stack: 'Total',
+        data: [155, 120, 150, 170, 180, 120, 200]
+      },
+      {
+        name: 'Height',
+        type: 'line',
+        stack: 'Total',
+        data: [200, 155, 145, 140, 150, 160, 120]
+      },
+      {
+        name: 'BloodGlucose',
+        type: 'line',
+        stack: 'Total',
+        data: [120, 300, 400, 500, 460, 400, 400]
+      },
+      {
+        name: 'BloodPressure',
+        type: 'line',
+        stack: 'Total',
+        data: [120, 170, 120, 90, 98, 96, 80]
+      },
+    
+    ]
+  };
+  
+  
   title = 'dashboard';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,20 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [
+    NgxEchartsModule.forRoot({
+      /**
+       * This will import all modules from echarts.
+       * If you only need custom modules,
+       * please refer to [Custom Build] section.
+       */
+      echarts: () => import('echarts'), // or import('./path-to-my-custom-echarts')
+    }),
+  ],
+  imports: [
+    NgxEchartsModule.forRoot({ echarts }),
+  ],
+
 })
 export class AppModule { }
